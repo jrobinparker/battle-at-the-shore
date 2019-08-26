55461d203ffff4dee712cc990c18b7a2ab0074c7
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-08-26 09:23:51 -0700

    Added React Visibility Sensor to About component, added entrance animations

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
-export default function About() {
+class About extends React.Component {
+  state = {
+    visible: false
+  }
+
+  render() {
+    if (this.state.visible) {
+      const aboutCost = document.querySelector('.about-cost')
+      const aboutStyles = document.querySelectorAll('.about-styles')
+      aboutCost.classList.remove('hidden');
+      aboutCost.classList.add('fade-in-up');
+      aboutStyles.forEach(as => {
+        as.classList.remove('hidden');
+        as.classList.add('fade-in-up');
+      })
+    }
+
   return (
+    <VisibilitySensor
+      PartialVisibility
+      onChange={isVisible => {
+        this.setState({
+          visible: isVisible
+        })
+      }}>
     <div className="about" id="about">
       <div className="about-img">
         <img src={require('../assets/fight.jpg')} />
       </div>
       <div className="about-info">
-        <div className="about-cost">
+        <div className="about-cost hidden">
           <div className="cost-info">
             <h1>$30</h1>
             <div>1 division</div>
@@ -21,17 +45,18 @@ export default function About() {
             <div>start time</div>
           </div>
       </div>
-      <div className="about-styles">
+
+      <div className="about-styles hidden">
           <div className="style-icon">
             <img src={require('../assets/padded-fighter.svg')} />
             <div>padded sticks</div>
           </div>
           <div className="style-icon">
-            <img src={require('../assets/knives.svg')} class="icon" />
+            <img src={require('../assets/knives.svg')} />
             <div>knife fighting</div>
           </div>
       </div>
-      <div className="about-styles">
+      <div className="about-styles hidden">
           <div className="style-icon">
             <img src={require('../assets/forms.svg')} />
             <div>forms</div>
@@ -40,8 +65,13 @@ export default function About() {
             <img src={require('../assets/toya.svg')} />
             <div>single & double sticks</div>
           </div>
-        </div>
+      </div>
+
       </div>
     </div>
+    </VisibilitySensor>
   )
+  }
 }
+
+export default About
