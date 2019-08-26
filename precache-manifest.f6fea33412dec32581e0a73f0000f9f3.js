ddffca2cb83863637c9391e5f8381e7266878580
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-08-26 13:43:42 -0700

    Updates

---
+++ precache-manifest.f6fea33412dec32581e0a73f0000f9f3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb7477f8f30a8a8de70a53fda695d2e",
+    "url": "/battle-at-the-shore/index.html"
+  },
+  {
+    "revision": "ed0e0c38cd2c2cabdd59",
+    "url": "/battle-at-the-shore/static/css/main.3059ba04.chunk.css"
+  },
+  {
+    "revision": "023737a8dd876d9f5f16",
+    "url": "/battle-at-the-shore/static/js/2.6c105a5f.chunk.js"
+  },
+  {
+    "revision": "ed0e0c38cd2c2cabdd59",
+    "url": "/battle-at-the-shore/static/js/main.69e42ab0.chunk.js"
+  },
+  {
+    "revision": "49a5cc910986387d08d6",
+    "url": "/battle-at-the-shore/static/js/runtime~main.720831c9.js"
+  },
+  {
+    "revision": "7cf15d9c0d4ee60a2a205d1c7e5080f2",
+    "url": "/battle-at-the-shore/static/media/bats-banner-1.7cf15d9c.gif"
+  },
+  {
+    "revision": "e7651565b9c935306d61de58a474d66c",
+    "url": "/battle-at-the-shore/static/media/contact.e7651565.jpg"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/battle-at-the-shore/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "c1d0ef71307cfc688973a36f73a2d1a6",
+    "url": "/battle-at-the-shore/static/media/fight.c1d0ef71.jpg"
+  },
+  {
+    "revision": "aa4ea082fc8134240e7bc3095654b18f",
+    "url": "/battle-at-the-shore/static/media/flyer.aa4ea082.jpg"
+  },
+  {
+    "revision": "28757da1d6ff4d0710d7cfa27cdc9066",
+    "url": "/battle-at-the-shore/static/media/forms.28757da1.svg"
+  },
+  {
+    "revision": "15bc5470aea6d2d05200dff45c14e92b",
+    "url": "/battle-at-the-shore/static/media/instagram.15bc5470.svg"
+  },
+  {
+    "revision": "4c8aa49ffd41662a0fe781a93c503fc8",
+    "url": "/battle-at-the-shore/static/media/knives.4c8aa49f.svg"
+  },
+  {
+    "revision": "bc3a2de4ebe5957b9a539402e947f17a",
+    "url": "/battle-at-the-shore/static/media/logo-transparent.bc3a2de4.png"
+  },
+  {
+    "revision": "e1e7bd85108d3aaa71097f07cfe52938",
+    "url": "/battle-at-the-shore/static/media/padded-fighter.e1e7bd85.svg"
+  },
+  {
+    "revision": "01ac5539b5b251a8a4fd980dc87fc27b",
+    "url": "/battle-at-the-shore/static/media/sensei-img.01ac5539.jpg"
+  },
+  {
+    "revision": "aad787b5e303a86f29de84da84b4cc2b",
+    "url": "/battle-at-the-shore/static/media/toya.aad787b5.svg"
+  },
+  {
+    "revision": "a51ab7ed010a415170cd93a2a29d96dd",
+    "url": "/battle-at-the-shore/static/media/youtube.a51ab7ed.svg"
+  }
+]);
