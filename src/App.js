ef2d39a1fe8f466f5c2d9e821253283b3c77b9c8
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-08-25 19:59:30 -0700

    Home Home component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import Home from './components/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div class="landing">
+      <div class="hero-banner">
+        <div class="banner-text">
+          <h1 class="title">Battle at the Shore</h1>
+          <h2 class="subtitle">Eskrima Invitational</h2>
+          <div class="banner-buttons">
+            <div class="banner-button">About</div>
+            <div class="banner-button">Register</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
