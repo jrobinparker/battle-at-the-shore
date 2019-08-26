0a5dc0172d642131bc4ea3cd8ebfde490b26d92e
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-08-26 14:23:31 -0700

    Updates

--- precache-manifest.88cf0d3ad96d103c40ee073b1d1d0339.js
+++ precache-manifest.88cf0d3ad96d103c40ee073b1d1d0339.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38df831893348d8d9e05b3affa3006d8",
+    "revision": "31692da6f49a5127dd8b6e581c073404",
     "url": "/battle-at-the-shore/index.html"
   },
   {
-    "revision": "3057f30244485597b537",
-    "url": "/battle-at-the-shore/static/css/main.5eb0bf71.chunk.css"
+    "revision": "475cb907a20996d78e3e",
+    "url": "/battle-at-the-shore/static/css/main.448cafa8.chunk.css"
   },
   {
     "revision": "023737a8dd876d9f5f16",
     "url": "/battle-at-the-shore/static/js/2.6c105a5f.chunk.js"
   },
   {
-    "revision": "3057f30244485597b537",
+    "revision": "475cb907a20996d78e3e",
     "url": "/battle-at-the-shore/static/js/main.058039fa.chunk.js"
   },
   {
